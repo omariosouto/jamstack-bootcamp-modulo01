d8d90ffa43fb1ec4bcd2e801ac328282659f75d3
Mario Souto <soutomarios@gmail.com>
2021-01-15 17:19:59 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
 import { Text } from '../src/components/foundation/Text';
@@ -77,6 +78,7 @@ export default function Home() {
               justifyContent={{ xs: 'center', md: 'flex-start' }}
             >
               <img
+                alt="Descrição do projeto"
                 style={{
                   maxWidth: '100%',
                   height: 'auto',
